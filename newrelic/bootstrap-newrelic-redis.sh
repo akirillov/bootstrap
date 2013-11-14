dea6e52a1ead366f223078c1f3dbf966375a8182
Anton Kirillov <akirillov@zeptolab.com>
2013-11-14 14:51:00 +0400

    (~) Redis plugin sed bugfix

--- newrelic/bootstrap-newrelic-redis.sh
+++ newrelic/bootstrap-newrelic-redis.sh
@@ -14,7 +14,8 @@ apt-get update
 
 pip install newrelic-plugin-agent
 
-sed -e "s/REPLACE_WITH_REAL_KEY/$LICENSE_KEY/g" /opt/newrelic_plugin_agent/newrelic_plugin_agent.cfg
+sed -e "s/REPLACE_WITH_REAL_KEY/$LICENSE_KEY/g" /opt/newrelic_plugin_agent/newrelic_plugin_agent.cfg > /tmp/newrelic_plugin_agent.cfg.tmp
+mv /tmp/newrelic_plugin_agent.cfg.tmp /opt/newrelic_plugin_agent/newrelic_plugin_agent.cfg
 
 echo "Now You need to edit newrelic_plugin_agent.cfg for proper configuration of monitred services."
 echo "Enjoy!"
