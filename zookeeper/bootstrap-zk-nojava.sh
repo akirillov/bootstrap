e2468220ba95ed2c6b20e4e836e9c82c91bba4b1
Anton Kirillov <akirillov@admoment.ru>
2014-03-14 18:15:51 +0400

    (~) start script output redirected to /dev/null

--- zookeeper/bootstrap-zk-nojava.sh
+++ zookeeper/bootstrap-zk-nojava.sh
@@ -16,7 +16,7 @@ wget https://raw.github.com/Netflix/exhibitor/master/exhibitor-standalone/src/ma
 mvn assembly:single
 
 #Generating startup script for exhibitor
-echo -e "local_ip=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`\necho \$local_ip \njava -jar target/exhibitor-1.0-jar-with-dependencies.jar -c file --hostname \$local_ip &" >> start.sh
+echo -e "local_ip=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`\necho \$local_ip \njava -jar target/exhibitor-1.0-jar-with-dependencies.jar -c file --hostname \$local_ip > /dev/null 2> /dev/null &" >> start.sh
 
 chmod +x start.sh
 
